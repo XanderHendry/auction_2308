78126b78a74a895294c82ea5d934f4207ab5e9b4
Xander Hendry <vhendry.coding@gmail.com>
2023-09-26 11:47:15 -0400

    Add add_bid & current_high_bid Item method spec tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -5,5 +5,27 @@ RSpec.describe Item do
   it 'can initialize' do 
     expect(item).to be_a(Item)
     expect(item.name).to eq("Chalkware Piggy Bank")
+    expect(item.bids).to eq({})
+  end
+  context 'adding and tracking bids' do
+    let(:attendee1) { Attendee.new({name: 'Megan', budget: '$50'}) }
+    let(:attendee2) { Attendee.new({name: 'Bob', budget: '$75'}) }
+    describe '#add_bid' do 
+      it 'will add a bid to the Items bid hash, with an Attendee as the key, and the bid as the value' do
+        item.add_bid(attendee2, 20)
+        item.add_bid(attendee1, 22)
+        expect(item.bids).to eq({
+          attendee1 => 20,
+          attendee2 => 22
+        })
+      end
+    end
+    describe '#current_high_bid' do 
+      it 'will return the current highest bid as an integer' do 
+        item.add_bid(attendee2, 20)
+        item.add_bid(attendee1, 22)
+        expect(item.current_high_bid).to eq(22)
+      end
+    end
   end
 end 
