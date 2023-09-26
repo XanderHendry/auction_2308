7d5348c272e38f277e8dfc2e5b4fb70dd7e23816
Xander Hendry <vhendry.coding@gmail.com>
2023-09-26 11:40:08 -0400

    Add unpopular_items & potential_revenue Auction method spec tests

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 RSpec.describe Auction do 
   let(:item1) { Item.new('Chalkware Piggy Bank') }
   let(:item2) { Item.new('Bamboo Picture Frame') } 
+  let(:item3) { Item.new('Homemade Chocolate Chip Cookies') } 
+  let(:item4) { Item.new('2 Days Dogsitting') } 
+  let(:item5) { Item.new('Forever Stamps') }
+  let(:attendee1) { Attendee.new({name: 'Megan', budget: '$50'}) }
+  let(:attendee2) { Attendee.new({name: 'Bob', budget: '$75'}) }
+  let(:attendee3) { Attendee.new({name: 'Mike', budget: '$100'}) }
   let(:auction) { Auction.new } 
   
   it 'can initialize' do 
@@ -9,7 +15,7 @@ RSpec.describe Auction do
     expect(auction.items).to eq([])
   end
   
-  describe '#Add item' do 
+  describe '#add item' do 
     it 'will add an Item to the Auction' do 
       auction.add_item(item1)
       expect(auction.items).to eq([item1])
@@ -18,12 +24,35 @@ RSpec.describe Auction do
     end
   end
 
-  describe '#item_names' do 
+  describe '#item names' do 
     it 'will return the name of each Item in the Auction' do 
       auction.add_item(item1)
       auction.add_item(item2)
       expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
     end
   end
+  context 'when checking bids on Items in the Auction'
+    before(:each) do 
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+      item4.add_bid(attendee3, 50)
+     end
+    describe '#unpopular items' do 
+      it 'will return items that have no bids' do 
+        expect(auction.unpopular_items).to eq([item2, item3, item5])
+        item3.add_bid(attendee2, 15)
+        expect(auction.unpopular_items).to eq([item2, item5])
+      end
+    end
+    describe '#potential revenue' do 
+      it 'will return the sum of all items highest bids' do 
+        expect(auction.potential_revenue).to eq(72)
+      end
+    end
+  end
 end
- 
