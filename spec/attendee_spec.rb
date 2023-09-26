e95d4c491b081e30f16b06f03f1bdce3cab2323d
Xander Hendry <vhendry.coding@gmail.com>
2023-09-26 11:24:31 -0400

    Implement Attendee class by passing tests

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -1,8 +1,14 @@
+require 'spec_helper'
 RSpec.describe Attendee do 
   let(:attendee) { Attendee.new({name: 'Megan', budget: '$50'}) } 
   it 'can initialize' do 
     expect(attendee).to be_a(Attendee)
     expect(attendee.name).to eq("Megan")
-    expect(attendee.budget).to eq(50)
+  end
+
+  describe '#budget' do 
+    it 'will return the Attendees budget as an integer' do 
+      expect(attendee.budget).to eq(50)
+    end
   end
 end 
